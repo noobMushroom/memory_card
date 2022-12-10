e745d125bbe3bf99458217755884ef1744785305
mushroom <mushroom019.coder@gmail.com>
2022-12-10 20:35:06 +0530

    changed bacground color

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -9,8 +9,8 @@ export default function Board(props: BoardProps) {
   const { state, handleClick } = props;
   return (
     <div className=" flex justify-center  pt-[5rem] ">
-      <div className="fixed bottom-0 left-0 right-0 top-0 bg-yellow-400 -z-50 border-b-0 border-[10px] border-black" />
-      <div className="fixed bottom-0 left-0 right-0 top-0 -z-50 bg-black/5" />
+      <div className="fixed bottom-0 left-0 right-0 top-0 bg-yellow-500 -z-50 border-b-0 border-[10px] border-black" />
+      <div className="fixed bottom-0 left-0 right-0 top-0 -z-50 bg-black/10" />
       <div className="w-[70%]  p-[1rem] box-border flex flex-wrap justify-center">
         {state.map((element) => {
           return (
